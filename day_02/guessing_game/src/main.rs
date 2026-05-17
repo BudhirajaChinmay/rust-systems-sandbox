3f4b1308aff32e0e979f03f209a478ef8b724b5a
Chinmay <chinmay.k.budhiraja@gmail.com>
2026-05-17 16:01:03 +0530

    [Day 02] Guessing game with random number generator

--- day_02/guessing_game/src/main.rs
+++ day_02/guessing_game/src/main.rs
@@ -1,15 +1,35 @@
+use rand::RngExt;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    let secret_number = rand::rng().random_range(1..101);
 
-    let mut guess = String::new();
+    loop {
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        println!("Please input your guess.");
 
-    println!("You guessed: {guess}");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
 }
