139b4602a9b59ca1376e8f1141ef162604fbc7eb
Chinmay <chinmay.k.budhiraja@gmail.com>
2026-05-24 18:58:54 +0530

    [Day 04] use tuples

--- day_04/area_calcualtor/src/main.rs
+++ day_04/area_calcualtor/src/main.rs
@@ -1,10 +1,9 @@
 fn main() {
-    let height: u32 = 100;
-    let width: u32 = 500;
+    let rect1 = (100, 500);
 
-    println!("Area of the reactangle is {}", area(width, height));
+    println!("Area of the reactangle is {}", area(rect1));
 }
 
-fn area(height: u32, width: u32) -> u32 {
-    height * width
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
